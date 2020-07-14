1fec57b46b3d00f93238f95e453921bfba9f66e9
Edgar <aguirre.edgar567@gmail.com>
2020-07-14 03:12:11 -0500

    added package.json

--- subtract.test.js
+++ subtract.test.js
@@ -3,6 +3,5 @@ const subtract = require('./subtract')
 
 test('properly subtracts two numbers', () => {
     expect(
-        subtract(1, 2)
-        ).toBe(-1)
+        subtract(1, 2 ).toBe(-1)
 })
